3e6626f91e57a43f9a2a22a41cb2b3d2484e4ac7
Ramiro Cornidez <ramirocornidez@gmail.com>
2022-12-03 10:57:40 -0700

    Practice examples for Lottie library

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
+import { useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
 
 export default function App() {
+  const animation = useRef(null);
   return (
     <View style={styles.container}>
+      <LottieView 
+        autoPlay
+        ref={animation}
+        style={{
+          width: 600,
+          height: 200,
+        }}
+        source={require('./assets/128706-3-seconds-loader-beeboard.json')}
+      />
       <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title='Restart Animation'
+        onPress={() => {
+          animation.current?.reset();
+          animation.current?.play();
+        }}
+        />
       <StatusBar style="auto" />
     </View>
   );
